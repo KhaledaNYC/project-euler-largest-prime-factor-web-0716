58fc5055f0f33b1ea4f7a5713ec8f1e4942fc6d0
KhaledaNYC <khaleda.khan87@gmail.com>
2016-10-16 18:12:05 -0400

    largest prime factor

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,20 @@
-# Enter your procedural solution here!
+def prime(num)
+	i = 2
+	while i < num
+		if num  % i == 0
+			return false
+		end
+		i+=1
+	end
+	return true
+end
+
+def largest_prime_factor(num)
+  i = num - 1
+  while i > 0
+    if num % i == 0 && prime(i)
+    	return i
+    end
+    i -= 1
+  end
+end
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,27 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+  attr_accessor :num
+  def initialize(num)
+    @num = num
+  end
+  def prime(n)
+  	i = 2
+  	while i < n
+  		if n % i == 0
+  			return false
+  		end
+  		i+=1
+  	end
+  	return true
+  end
+
+  def number
+    i = num - 1
+    while i > 0
+      if num % i == 0 && prime(i)
+      	return i
+      end
+      i -= 1
+    end
+  end
+end
